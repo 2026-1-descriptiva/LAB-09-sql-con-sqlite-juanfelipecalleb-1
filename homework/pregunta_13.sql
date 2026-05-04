2d20ef207b4ebbddb0fb958a77a7a8aed00a2faf
juanfelipecalleb <jcalleb@unal.edu.co>
2026-05-04 00:00:26 -0500

    TRY2

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -43,9 +43,12 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
+
+
+
 SELECT 
     K0,
-    ROUND(AVG(c12), 2) AS "avg(c12)"
+    ROUND(AVG(c12) + 1e-9, 2) AS "avg(c12)"
 FROM tbl1
 WHERE c13 > 400
 GROUP BY K0
